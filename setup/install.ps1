20cd21d9032930d2b1220fe963d33f0ccffaa47d
Jonas Hoefflin User <jh.jonas@web.de>
2021-03-04 09:31:07 +0100

    stop closing windows

--- setup/install.ps1
+++ setup/install.ps1
@@ -1,21 +1,21 @@
 $account = "hoeffjo"
-$repo    = "setup-dotfiles-choco"
-$branch  = "master"
+$repo = "setup-dotfiles-choco"
+$branch = "master"
 
 $dotfilesTempDir = Join-Path $env:TEMP "dotfiles"
-if (![System.IO.Directory]::Exists($dotfilesTempDir)) {[System.IO.Directory]::CreateDirectory($dotfilesTempDir)}
+if (![System.IO.Directory]::Exists($dotfilesTempDir)) { [System.IO.Directory]::CreateDirectory($dotfilesTempDir) }
 $sourceFile = Join-Path $dotfilesTempDir "dotfiles.zip"
 $dotfilesInstallDir = Join-Path $dotfilesTempDir "$repo-$branch"
 
 
 function Download-File {
-  param (
-    [string]$url,
-    [string]$file
-  )
-  Write-Host "Downloading $url to $file"
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  Invoke-WebRequest -Uri $url -OutFile $file
+    param (
+        [string]$url,
+        [string]$file
+    )
+    Write-Host "Downloading $url to $file"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $url -OutFile $file
 
 }
 
@@ -36,21 +36,24 @@ function Unzip-File {
         try {
             [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
             [System.IO.Compression.ZipFile]::ExtractToDirectory("$filePath", "$destinationPath")
-        } catch {
+        }
+        catch {
             Write-Warning -Message "Unexpected Error. Error details: $_.Exception.Message"
         }
-    } else {
+    }
+    else {
         try {
             $shell = New-Object -ComObject Shell.Application
             $shell.Namespace($destinationPath).copyhere(($shell.NameSpace($filePath)).items())
-        } catch {
+        }
+        catch {
             Write-Warning -Message "Unexpected Error. Error details: $_.Exception.Message"
         }
     }
 }
 
 Download-File "https://github.com/$account/$repo/archive/$branch.zip" $sourceFile
-if ([System.IO.Directory]::Exists($dotfilesInstallDir)) {[System.IO.Directory]::Delete($dotfilesInstallDir, $true)}
+if ([System.IO.Directory]::Exists($dotfilesInstallDir)) { [System.IO.Directory]::Delete($dotfilesInstallDir, $true) }
 Unzip-File $sourceFile $dotfilesTempDir
 
 Push-Location $dotfilesInstallDir
@@ -60,5 +63,4 @@ Pop-Location
 $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
 $newProcess.Arguments = "-nologo";
 [System.Diagnostics.Process]::Start($newProcess);
-exit
 
